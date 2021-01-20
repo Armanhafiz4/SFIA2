e8f0d139ed058ff1f80ed6a829a837d15c66b40d
Armanhafiz4 <ahafiz15@hotmail.com>
2021-01-20 16:05:39 +0000

    new committs

--- Codes/service_1/application/routes.py
+++ Codes/service_1/application/routes.py
@@ -1,12 +1,14 @@
 from application import app 
+from flask import render_template 
 import requests
 
 @app.route('/')
 def index(): 
-    strength_response = requests.get("http://player-backend:5000/strength")
-    weakness_response = requests.get("http://player-backend:5000/weakness")
-    position_response = requests.post("http://player-backend:5000/position", json={"strength" : strength_response.text, "weakness" : weakness_response.text})
-    return render_template("index.html", strength=strength_response.text, weakness=weakness_response.text, position=position_response.get_json())
+    strength_response = requests.get("http://football_gen_service_2:5000/strength")
+    weakness_response = requests.get("http://football_gen_service_3:5000/weakness")
+    position_response = requests.post("http://football_gen_service_4:5000/position", json={"strength" : strength_response.text, "weakness" : weakness_response.text})
+    app.logger.info(position_response.json())
+    return render_template("index.html", strength=strength_response.text, weakness=weakness_response.text, position=position_response.json())
     
 
 
--- Codes/service_4/application/routes.py
+++ Codes/service_4/application/routes.py
@@ -7,4 +7,6 @@ def get_position():
     strength = request.get_json()["strength"]
     unsuited_positions = { "passing" : "striker", "dribbling" : "centreback", "heading" : "winger"}
     weakness = request.get_json()["weakness"]
+    app.logger.info(strength)
+    app.logger.info(weakness)
     return jsonify({ "suited_position" : suited_positions[strength], "unsuited_position" : unsuited_positions[weakness] })
