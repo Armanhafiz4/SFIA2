46622deb45d82700c455ea26930aa80f473306a9
Armanhafiz4 <ahafiz15@hotmail.com>
2021-01-22 13:06:09 +0000

    tests commit

--- Codes/service_1/tests/test_unit.py
+++ Codes/service_1/tests/test_unit.py
@@ -26,7 +26,8 @@ class TestResponse(TestBase):
         with requests_mock.mock() as m:
             m.get('http://football_gen_service_2:5000/strength', text="pace")
             m.get('http://football_gen_service_3:5000/weakness' ,text="dribbling")
-            m.post('http://football_gen_service_4:5000/position', json={"pace" : "winger", "dribbling" : "centreback"})
+            m.post('http://football_gen_service_4:5000/position', json={"suited_position" : "winger", "unsuited_position" : "centreback"})
             response = self.client.get(url_for('index'))
             self.assertIn(b'pace', response.data)
-            self.assertIn(b'"suited_position:"winger","unsuited_position":centreback"', response.data)
+            self.assertIn(b'Your best trait is your pace so your best position will be as a winger', response.data)
+            self.assertIn(b'Your worst trait is your dribbling and therefore your worst position would be as a centreback', response.data)
