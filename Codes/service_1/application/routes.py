2f38053500915be89d0ff71283cf6414b2b5fdda
Armanhafiz4 <ahafiz15@hotmail.com>
2021-01-22 10:05:24 +0000

    database is completed

--- Codes/service_1/application/routes.py
+++ Codes/service_1/application/routes.py
@@ -1,6 +1,12 @@
-from application import app 
+from application import app, db  
 from flask import render_template 
 import requests
+
+class Positions(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    strength_type = db.Column(db.String(20), nullable=False)
+    weakness_type = db.Column(db.String(20), nullable=False)
+    position_type = db.Column(db.String(20), nullable=False)
 
 @app.route('/')
 def index(): 
@@ -8,8 +14,15 @@ def index():
     weakness_response = requests.get("http://football_gen_service_3:5000/weakness")
     position_response = requests.post("http://football_gen_service_4:5000/position", json={"strength" : strength_response.text, "weakness" : weakness_response.text})
     app.logger.info(position_response.json())
-    return render_template("index.html", strength=strength_response.text, weakness=weakness_response.text, position=position_response.json())
-    
+
+    new_position = Positions(strength_type = strength_response.text, weakness_type = weakness_response.text, position_type = position_response.json()["suited_position"])
+    db.session.add(new_position)
+    db.session.commit()
+
+    all_positions = Positions.query.all()
+
+    return render_template("index.html", strength=strength_response.text, weakness=weakness_response.text, position=position_response.json(), all_positions=all_positions)
+
 
 
 
